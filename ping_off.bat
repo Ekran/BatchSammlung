eec3fd926363296946f5abc413230278c4a6f058
Ekran <Ekran@users.noreply.github.com>
2016-12-16 10:39:34 +0100

    Create ping_off.bat

---
+++ ping_off.bat
@@ -0,0 +1,7 @@
+rem im Terminal ausführen (Start - 'cmd' eingeben, rechtsklick auf Terminalsysmbol und "Als Administrator ausführen" oder rechtsklick auf Batch und "Als Admin. ausführen"
+
+rem netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=block
+
+netsh advfirewall firewall set rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in new action=block
+
+pause
