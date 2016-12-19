b7eecc10f9cd42603a0420db2c40e05c888839ec
Ekran <Ekran@users.noreply.github.com>
2016-12-19 10:45:10 +0100

    Create firewall_off.bat

---
+++ firewall_off.bat
@@ -0,0 +1,6 @@
+rem *** run as administrator / Als Adminstrator ausführen / mit Admin-Rechten ***
+
+netsh advfirewall set privateprofil state off
+netsh advfirewall set publicprofile state off
+
+pause
