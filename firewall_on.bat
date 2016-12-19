e25718377d27fb1a2a03e3c6b6af51adedc84c71
Ekran <Ekran@users.noreply.github.com>
2016-12-19 10:45:48 +0100

    Create firewall_on.bat

---
+++ firewall_on.bat
@@ -0,0 +1,7 @@
+rem *** run as administrator / Als Adminstrator ausführen / mit Admin-Rechten ***
+
+netsh advfirewall set privateprofil state on
+netsh advfirewall set publicprofile state on
+
+
+pause
