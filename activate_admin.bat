08a54742593cfb15f1a8b6cd42606cd99b1c4e42
Ekran <Ekran@users.noreply.github.com>
2016-12-20 07:50:04 +0100

    Create activate_admin.bat

---
+++ activate_admin.bat
@@ -0,0 +1,6 @@
+rem Administrator-Konto aktivieren und manuell passwort vergeben
+rem **** Mit Adminstraotrechten starten (rechtsklick, ausführe als Admin...)
+
+net user administrator * /active:yes
+
+pause
