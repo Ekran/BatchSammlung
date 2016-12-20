86d7be6cf95fee05fd377748d3c15e6cc71f91b1
Ekran <Ekran@users.noreply.github.com>
2016-12-20 08:05:10 +0100

    Update set_ntp_hszg.bat

--- set_ntp_hszg.bat
+++ set_ntp_hszg.bat
@@ -1,4 +1,13 @@
+rem ** run with admin priviledges
+
+rem newer windows systems
+w32tm /config /syncfromflags:manual /manualpeerlist:"ntp.hszg.de"
+w32tm /config /update
+
+rem on older systems like 2003
 net time /setsntp:"ntp.hszg.de"
+
+rem found on http://www.pool.ntp.org/de/use.html
 
 net stop w32time
 net start w32time
